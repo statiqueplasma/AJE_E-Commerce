80309b595a05844bae7159ecfbc1ad4ad582d8c1
Mohammed El Amine Rhazzar <mohammedelaminerhazzar@gmail.com>
2020-12-20 13:31:55 +0100

    .env file added

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,15 @@ require('dotenv/config');
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log("connected to db again!");
+  console.log("connected to db again!");
 });
 
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   const products = await Product.find();
-  res.render(products);
+  // res.json(products);
+  res.render('index', { title: 'Express', products: products });
 
 });
 
