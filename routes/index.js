bea6433aaadae6537efed2b3f8aff1053d4094d0
Meryem0107 <xenatwister0107@gmail.com>
2020-12-21 14:27:32 +0100

    user model

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/products');
+var csrf = require('csurf');
+
+var csrfProtection =csrf();
+router.use(csrfProtection);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var products = Product.find(function(err, obj){
     res.render('index', { title: 'Express' , products: obj});
   });
   
+});
+router.get('/user/signup', function(req, res, next) {
+  res.render('user/signup', {csrfToken: req.csrfToken()});
 });
 
 module.exports = router;
