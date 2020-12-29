44a31e41852272f462ed3c49bea8ebad0a535461
Mohammed El Amine Rhazzar <mohammedelaminerhazzar@gmail.com>
2020-12-29 12:41:12 +0100

    pagination fixed

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const products = require('../models/products');
 var router = express.Router();
 
 var { product } = require('../models/products');
@@ -60,25 +61,31 @@ const adjust = (obj) => {
 router.get('/', function (req, res) {
   res.redirect('/page/1');
 });
-router.get('/page/:page', async function (req, res) {
-  var products = await product.find().lean();
-  try {
-    var curpage = req.params.page;
-    var numItem = 30;
-    var numbPages = Math.trunc(products.length / numItem) + 1;
-    var pages = { previous: 0, current: parseInt(curpage, 10), next: 0, numb: numbPages };
-    products = products.slice((curpage - 1) * numItem, curpage * numItem)
-    if (curpage > 1) {
-      pages.previous = parseInt(curpage, 10) - 1;
+router.get('/page/:page', (req, res) => {
+  product.find().lean().exec((err, products) => {
+    try {
+      var curpage = req.params.page;
+      var numItem = 30;
+      var numbPages = Math.trunc(products.length / numItem) + 1;
+      var pages = {
+        previous: 0,
+        current: parseInt(curpage, 10),
+        next: 0,
+        numb: numbPages
+      };
+      products = products.slice((curpage - 1) * numItem, curpage * numItem)
+      if (curpage > 1) {
+        pages.previous = parseInt(curpage, 10) - 1;
+      }
+      if (curpage < numbPages) {
+        pages.next = parseInt(curpage, 10) + 1;
+      }
+      res.render('index', { title: 'Express', products: products, pages: pages });
     }
-    if (curpage < numbPages) {
-      pages.next = parseInt(curpage, 10) + 1;
+    catch (err) {
+      console.log(err);
     }
-    res.render('index', { title: 'Express', products: products, pages: pages });
-  }
-  catch (err) {
-    console.log(err);
-  }
+  });
 });
 
 
@@ -94,9 +101,11 @@ router.post('/', (req, res) => {
         { snippet: keywords },
         { description: keywords }
       ]
-    }, "colors category").lean().exec((err, products) => {
-      res.render('index', { title: 'Express', products: products });
     })
+      .lean()
+      .exec((err, products) => {
+        res.render('index', { title: 'Express', products: products });
+      })
 
   } else {
     var queryParams = adjust(req.body);
